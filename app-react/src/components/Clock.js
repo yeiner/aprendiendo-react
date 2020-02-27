36e221a8f8ef59bf5cab7755fa7505045c0d3d62
Yeiner Morales <yeimor@eltiempo.com>
2020-02-27 16:52:40 -0500

    Agregar métodos de ciclo de vida a una clase

--- app-react/src/components/Clock.js
+++ app-react/src/components/Clock.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react";
 
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-    render() {
-        return(
-            <div>
-                <h2>It is {this.state.date.toLocaleTimeString()} .</h2>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  /**Metodo de ciclo de vida. Montaje.
+        Se ejecuta después que la salida del componente ha sido renderizada con el DOM. 
+    ***/
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  //Metodo de ciclo de vida. Desmontaje.
+  componentWillUnmount() {
+      clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+        date : new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()} .</h2>
+      </div>
+    );
+  }
 }
 
 export default Clock;
