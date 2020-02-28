dbe668fd57b8077ad86eb4a143c5991ef6590b68
Yeiner Morales <yeimor@eltiempo.com>
2020-02-28 15:38:01 -0500

    >Manejando eventos :stuck_out_tongue_closed_eyes:

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Clock from './components/Clock'
+import Clock from './components/Clock';
+import Toggle from './components/Toggle';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, {formatName(user)}</h1>
       <Clock />
+      <div>
+        <h2>Manejando eventos</h2>
+        <Toggle />
+      </div>
     </div>
   );
 }
